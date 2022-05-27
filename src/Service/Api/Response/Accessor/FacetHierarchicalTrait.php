586fcb1201c584abc55b1bc22f7b95d0b3f80435
danneg <dana@boxalino.com>
2022-05-27 22:15:46 +0200

    [facet][categories] update flow on which API response facet options to be displayed

--- src/Service/Api/Response/Accessor/FacetHierarchicalTrait.php
+++ src/Service/Api/Response/Accessor/FacetHierarchicalTrait.php
@@ -8,6 +8,10 @@ namespace Boxalino\RealTimeUserExperienceApi\Service\Api\Response\Accessor;
  * The properties defined are a base on what can be configured in Boxalino Intelligence Admin
  * The rest of the properties (custom defined) can be accessed directly
  *
+ * Sample of hierarchical facet: categories
+ * a) identify the 1st parent with more than 1 children & show all kids
+ * b) identifies the selected facet & shows only it`s kids
+ *
  * @package Boxalino\RealTimeUserExperienceApi\Service\Api\Response\Accessor
  */
 trait FacetHierarchicalTrait
@@ -27,6 +31,8 @@ trait FacetHierarchicalTrait
             $this->_buildTree($value, 0);
         }
 
+        $this->_createLevelWithValidChildren();
+
         return $this;
     }
 
@@ -36,7 +42,7 @@ trait FacetHierarchicalTrait
      * @param int $level
      * @return void
      */
-    protected function _buildTree($value, int $level)
+    protected function _buildTree($value, int $level, ?string $parentId = null)
     {
         $value->label = [$value->label];
         if($level > 1)
@@ -44,28 +50,63 @@ trait FacetHierarchicalTrait
             $value->path[] = $value->label[0];
         }
 
+        $this->_createFacetValueEntity($value, $level, $parentId);
         if(count($value->children))
         {
             $level++;
             foreach($value->children as $index => $child)
             {
                 $child->path = $value->path;
-                $this->_buildTree($child, $level);
+                $this->_buildTree($child, $level, $value->value);
             }
-
-            return;
         }
+    }
 
-        /** only create facetValueEntities for the last level of hierarchy */
+    /**
+     * @param $value
+     * @param $level
+     * @param $parentId
+     * @return void
+     */
+    protected function _createFacetValueEntity($value, int $level, ?string $parentId = null) : void
+    {
+        /** only create facetValueEntities excluding the first 2 parents from hierarchy (ex: root & website level) */
         if($level > 1)
         {
             /** @var FacetValue $facetValueEntity */
             $facetValueEntity = $this->toObject($value, $this->getAccessorHandler()->getAccessor("facetValue"));
             $facetValueEntity->setId([$facetValueEntity->getValue()]);
-            $facetValueEntity->set("level", $level);
+            $facetValueEntity->set("level", (string)$level);
+            $facetValueEntity->set("parent", (string)$parentId);
 
             $this->values->append($facetValueEntity);
         }
+    }
+
+    /**
+     * 1. If there is a selected facet option -> show the children from it only
+     * 2. If there is no selected facet option -> show the lowelest level with more than one kid
+     *
+     * @return void
+     */
+    protected function _createLevelWithValidChildren() : void
+    {
+        $facetValues = [];
+        array_map(function(AccessorInterface $facetValue) use (&$facetValues) {
+            $facetValues[$facetValue->getLevel()][] = $facetValue->getValue();
+            if($facetValue->isSelected())
+            {
+                $facetValues["selected"] = $facetValue->getValue();
+            }
+        }, $this->getValues()->getArrayCopy());
+
+        if(isset($facetValues["selected"]))
+        {
+            $this->_setChildrenByRules(["parent"=>$facetValues["selected"],"value"=>$facetValues["selected"]]);
+            return;
+        }
+
+        $this->_setChildren("level", $this->_getLevel($facetValues));
     }
 
     /**
@@ -107,30 +148,70 @@ trait FacetHierarchicalTrait
         }
     }
 
-
     /**
-     * @param string $field
-     * @return string
+     * @param array $valueRules
+     * @param array $showRules
+     * @return void
      */
-    protected function _apiMapField(string $field) : string
+    protected function _setChildrenByRules(array $valueRules) : void
     {
-        if(in_array($field, array_keys($this->_getAlternativeFacetMapping())))
+        $values = new \ArrayIterator();
+        foreach($valueRules as $property => $value)
         {
-            return $this->_getAlternativeFacetMapping()[$field];
+            $this->_addFacetValueToCollection($values, $property, $value);
         }
 
-        return $field;
+        $this->values = $values;
     }
 
     /**
-     * A list of API properties that have alternatives
-     * (ex: categories)
-     *
-     * @return string[]
+     * @param string $property
+     * @param string $value
+     * @return void
      */
-    protected function _getAlternativeFacetMapping() : array
+    protected function _setChildren(string $property, string $value) : void
     {
-        return [Facet::RTUX_API_FACET_CATEGORIES => Facet::RTUX_API_FACET_CATEGORY];
+        $values = new \ArrayIterator();
+        $this->_addFacetValueToCollection($values, $property, $value);
+
+        $this->values = $values;
+    }
+
+    /**
+     * @param \ArrayIterator $values
+     * @param string $property
+     * @param string $value
+     * @return void
+     */
+    protected function _addFacetValueToCollection(\ArrayIterator &$values, string $property, string $value) : void
+    {
+        foreach($this->getValues() as $facetValue)
+        {
+            if($facetValue->get($property) === $value)
+            {
+                $values->append($facetValue);
+            }
+        }
+    }
+
+    /**
+     * @param array $list
+     * @return string
+     */
+    protected function _getLevel(array $list) : string
+    {
+        ksort($list, SORT_NATURAL);
+        $levels = [];
+        array_map(function ($level, $values) use (&$levels){
+            if(is_array($values) && count($values) > 1)
+            {
+                $levels[$level] = count($values);
+            }
+        }, array_keys($list), $list);
+
+        reset($levels);
+
+        return (string)key($levels);
     }
 
 
