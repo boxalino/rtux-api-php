26ee5cf62395fd4bdebfd6fbb50ea327b336b75c
danneg <dana@boxalino.com>
2023-05-19 16:24:01 +0200

    [3.3.33] API domain update bx-cloud to alternative

--- src/Service/Api/Util/ConfigurationInterface.php
+++ src/Service/Api/Util/ConfigurationInterface.php
@@ -9,12 +9,14 @@ namespace Boxalino\RealTimeUserExperienceApi\Service\Api\Util;
  */
 interface ConfigurationInterface
 {
-    public const RTUX_API_ENDPOINT_PRODUCTION="https://main.bx-cloud.com/narrative/%%account%%/api/1";
-    public const RTUX_API_ENDPOINT_STAGE="https://r-st.bx-cloud.com/narrative/%%account%%/api/1";
+    public const RTUX_API_ENDPOINT_PRODUCTION="https://main.%%domain%%.com/narrative/%%account%%/api/1";
+    public const RTUX_API_ENDPOINT_STAGE="https://r-st.%%domain%%.com/narrative/%%account%%/api/1";
     public const BOXALINO_API_TRACKING_PRODUCTION="//track.bx-cloud.com/static/bav2.min.js";
     public const BOXALINO_API_TRACKING_STAGE="//r-st.bx-cloud.com/static/bav2.min.js";
     public const BOXALINO_API_SERVER_PRODUCTION="//track.bx-cloud.com/track/v2";
     public const BOXALINO_API_SERVER_STAGE="//r-st.bx-cloud.com/track/v2";
+    public const RTUX_API_DOMAIN_MAIN="bx-cloud";
+    public const RTUX_API_DOMAIN_ALTERNATIVE="wi-platform-cloud";
 
     /**
      * The API endpoint depends on the testing conditionals and on the data index
